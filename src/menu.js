f8ca43455760f0c234499c1046ce69332ce92d19
Mats <mats.kvanvik@hotmail.com>
2024-07-04 18:21:50 +0200

    moved menu items to JSON format

--- src/menu.js
+++ src/menu.js
@@ -1,27 +1,44 @@
 
-const items = [
+const menuItems = [
     {
-        name:
+        name: "Mango Madness Smoothie",
+        description: "Dive into a tropical paradise with our Mango Madness Smoothie. Made with fresh mangoes, a hint of pineapple, a splash of coconut milk, and a dash of honey, this smoothie is perfect for a refreshing treat anytime.",
+        price: "$6.50"
+    },
+    {
+        name: "Spicy Mango Salsa",
+        description: "Add a zesty kick to your snack time with our Spicy Mango Salsa. This vibrant mix of ripe mangoes, jalapeños, red onions, cilantro, and lime juice is perfect for dipping chips or topping your favorite dishes.",
+        price: "$4.99"
+    },
+    {
+        name: "Mango Coconut Tart",
+        description: "Indulge in our Mango Coconut Tart, a delightful dessert featuring a buttery crust filled with creamy coconut custard and topped with luscious slices of fresh mango. Finished with a sprinkle of toasted coconut flakes.",
+        price: "$5.75"
+    },
+    {
+        name: "Mango Avocado Salad",
+        description: "Enjoy a healthy and delicious meal with our Mango Avocado Salad. This refreshing salad combines juicy mangoes, creamy avocados, mixed greens, cherry tomatoes, and red onion, all tossed in a tangy lime vinaigrette.",
+        price: "$8.99"
     }
 ]
 
-const createMenuItem = (name, description, price) => {
+const createMenuItem = (item) => {
 
     const menuItem = document.createElement("li");
 
 
     const itemName = document.createElement('h2');
-    itemName.innerText = name;
+    itemName.innerText = item.name;
     menuItem.appendChild(itemName);
 
     //Add price to list item
     const itemPrice = document.createElement("p")
-    itemPrice.innerText = price
+    itemPrice.innerText = item.price
     menuItem.appendChild(itemPrice);
 
     //Add description to list item
     const itemDescription = document.createElement('p');
-    itemDescription.innerText = description;
+    itemDescription.innerText = item.description;
     menuItem.appendChild(itemDescription);
 
 
@@ -41,9 +58,11 @@ const menu = () => {
 
     const menuList = document.createElement("ul");
     menuList.classList.add("menu");
-    menuList.appendChild(createMenuItem("Mango Madness Smoothie", "Dive into a tropical paradise with our Mango Madness Smoothie. Made with fresh mangoes, a hint of pineapple, a splash of coconut milk, and a dash of honey, this smoothie is perfect for a refreshing treat anytime.", "$6.50"));
-    menuList.appendChild(createMenuItem("Spicy Mango Salsa", "Add a zesty kick to your snack time with our Spicy Mango Salsa. This vibrant mix of ripe mangoes, jalapeños, red onions, cilantro, and lime juice is perfect for dipping chips or topping your favorite dishes.", "$64.99"));
-    menuList.appendChild(createMenuItem("Mango Avocado Salad", "Enjoy a healthy and delicious meal with our Mango Avocado Salad. This refreshing salad combines juicy mangoes, creamy avocados, mixed greens, cherry tomatoes, and red onion, all tossed in a tangy lime vinaigrette.", "$8.99"));
+    menuItems.forEach(
+        item =>
+            menuList.appendChild(createMenuItem(item))
+    );
+    //menuList.appendChild(createMenuItem("Mango Madness Smoothie", "Dive into a tropical paradise with our Mango Madness Smoothie. Made with fresh mangoes, a hint of pineapple, a splash of coconut milk, and a dash of honey, this smoothie is perfect for a refreshing treat anytime.", "$6.50"));
     div.appendChild(menuList);
 
     return div;
